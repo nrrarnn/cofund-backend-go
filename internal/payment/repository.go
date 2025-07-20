8830aab39c80e120a298bd5b0c90890256cba640
Nur Arini <nurarini0302@gmail.com>
2025-07-20 18:59:26 +0700

    feat: add payment repository

--- internal/payment/repository.go
+++ internal/payment/repository.go
@@ -7,6 +7,9 @@ import (
 type PaymentRepository interface {
 	Create(payment *Payment) error
 	FindAll() ([]Payment, error)
+	Update(payment *Payment) error
+	Delete(id uint) error
+	FindByID(id uint) (*Payment, error)
 }
 
 type paymentRepo struct {
@@ -25,4 +28,18 @@ func (r *paymentRepo) FindAll() ([]Payment, error) {
 	var payments []Payment
 	err := r.db.Find(&payments).Error
 	return payments, err
+}
+
+func (r *paymentRepo) Update(payment *Payment) error {
+	return r.db.Save(payment).Error
+}
+
+func (r *paymentRepo) Delete(id uint) error {
+	return r.db.Delete(&Payment{}, id).Error
+}
+
+func (r *paymentRepo) FindByID(id uint) (*Payment, error) {
+	var payment Payment
+	err := r.db.First(&payment, id).Error
+	return &payment, err
 }
